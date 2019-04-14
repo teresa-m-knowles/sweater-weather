b9c45440bf7bf660bc68e5d547537056eaa4c31b
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-14 17:44:49 -0600

    creates Favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_225055) do
+ActiveRecord::Schema.define(version: 2019_04_14_234348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_favorites_on_location_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "city_and_state"
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2019_04_14_225055) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "locations"
+  add_foreign_key "favorites", "users"
 end
---
+++ spec/requests/api/v1/user_can_favorite_a_location_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'User can favorite a location', type: :request do
+  context 'when a user sends in a location and a valid api_key' do
+    it 'saves that location to its favorites' do
+    end
+  end
+end
