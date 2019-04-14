85325c5edc9518b6e5370b7e824be17defcfa5ff
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-14 16:31:22 -0600

    got rid of session, not needed for now

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(user_params[:email])
     if user && user.authenticate(user_params[:password])
-      session[:user_id] = user.id
       render status: 200, json: {
                                   "api_key": "#{user.api_key}"
                                 }
