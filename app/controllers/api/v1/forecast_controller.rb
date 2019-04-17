db6c1198eb5a30c8c495d5aa1a9c4253b8a14bcb
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-17 12:33:59 -0600

    cache try

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ForecastController < Api::V1::BaseController
   def show
     location = LocationFacade.new(params[:location])
 
-    render json: LocationFacadeSerializer.new(location)
+    render json: LocationFacadeSerializer.new(location), cached: true
   end
 end
